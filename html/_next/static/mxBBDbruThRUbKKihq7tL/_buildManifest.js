3982972e306072eba22def75e787ee52b9d42c01
kittipoom.p <kittipoom@feyverly.com>
2021-03-10 16:55:24 +0700

    build html

---
+++ html/_next/static/mxBBDbruThRUbKKihq7tL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6fe462dc622ceff9d6e1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fb77cdc4cfb459730ede.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
